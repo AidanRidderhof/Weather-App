b14024305ad2d70d3e1fbe6ab9f2d464e1bfc482
AidanRidderhof <aidan.ridderhof@gmail.com>
2025-10-24 12:32:49 -0700

    last bit of trimming

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin')
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
@@ -6,6 +8,11 @@ module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin()
+    ],
     usedExports: true, // Enable tree shaking
     splitChunks: {
       chunks: 'all',
